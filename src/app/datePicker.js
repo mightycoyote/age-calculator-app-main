e22024c40f0b5792cbcbbe9e96ee1f5d41f661f0
Sarah Wilkes <aremnius@gmail.com>
2023-11-03 11:36:39 -0400

    work on form

--- src/app/datePicker.js
+++ src/app/datePicker.js
@@ -1,22 +1,68 @@
+"use client";
+
+import { useState } from "react";
+
 import styles from "./datePicker.module.css";
 
+function validateEntry(day, month, year) {
+
+};
+
 function DatePicker() {
-    return (
-        <form className={styles.form}>
-            <div className={styles.dmy}>
-                <label htmlFor="day">Day</label>
-                <input id="day"></input>
-            </div>
-            <div className={styles.dmy}>
-                <label htmlFor="month">Month</label>
-                <input id="month"></input>
-            </div>
-            <div className={styles.dmy}>
-                <label htmlFor="year">Year</label>
-                <input id="year"></input>
-            </div>
-        </form>
-    )
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+
+  return (
+    <form onSubmit={event => {
+        event.preventDefault();
+        validateEntry(day, month, year);
+    }}>
+      <div className={styles.inputDiv}>
+        <div className={styles.dmy}>
+          <label htmlFor="day">Day</label>
+          <input
+            id="day"
+            size="4"
+            placeholder="DD"
+            required
+            // make these all regexes
+            value={day}
+            onChange={(event) => {
+              setDay(event.target.value);
+            }}
+          ></input>
+        </div>
+        <div className={styles.dmy}>
+          <label htmlFor="month">Month</label>
+          <input
+            id="month"
+            size="4"
+            placeholder="MM"
+            required
+            value={month}
+            onChange={(event) => {
+              setMonth(event.target.value);
+            }}
+          ></input>
+        </div>
+        <div className={styles.dmy}>
+          <label htmlFor="year">Year</label>
+          <input
+            id="year"
+            size="4"
+            placeholder="YYYY"
+            required
+            value={year}
+            onChange={(event) => {
+              setYear(event.target.value);
+            }}
+          ></input>
+        </div>
+      </div>
+      <button>arrow thing</button>
+    </form>
+  );
 }
 
 export default DatePicker;
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <DatePicker />
-      {/* ornamental dividing line & arrow */}
       <DateDisplay />
     </main>
   )

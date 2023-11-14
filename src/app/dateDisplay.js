91e2a6f3ff01ce5810739eabbec19d3c01ae8651
Sarah Wilkes <aremnius@gmail.com>
2023-11-14 16:23:37 -0500

    fix DateDisplay bug

--- src/app/dateDisplay.js
+++ src/app/dateDisplay.js
@@ -1,17 +1,18 @@
 import styles from "./dateDisplay.module.css";
 
 function DateDisplay( {elapsed} ) {
-    // parseInt() makes it possible to eliminate leading zeroes and show singular "day" etc
-    // however, now I have to figure out how to make it show 0 as needed instead of reverting to blanks
-    const years = parseInt(elapsed[0]);
-    const months = parseInt(elapsed[1]);
-    const days = parseInt(elapsed[2]);
+    // If I try to put the blanks (" - -") in State, React drops one.
+    // parseInt() eliminates leading zeroes, but when I had "years || " - -"" it would show the blanks if the value was 0
+    // adding "toFixed()" gives you a 0 when the value is 0, but then "NaN" when the page is first loaded
+    const years = parseInt(elapsed[0]).toFixed();
+    const months = parseInt(elapsed[1]).toFixed();
+    const days = parseInt(elapsed[2]).toFixed();
 
     return (
         <div className={styles.ymd}>
-            <p><span>{years || " - -"}</span> {years === 1 ? "year" : "years"}</p>
-            <p><span>{months || " - -"}</span> {months === 1 ? "month" : "months"}</p>
-            <p><span>{days || " - -"}</span> {days === 1 ? "day" : "days"}</p>
+            <p><span>{(years !== "NaN") ? years : " - -"}</span> {years === "1" ? "year" : "years"}</p>
+            <p><span>{(months !== "NaN") ? months : " - -"}</span> {months === "1" ? "month" : "months"}</p>
+            <p><span>{(days !== "NaN") ? days : " - -"}</span> {days === "1" ? "day" : "days"}</p>
         </div>
     )
 }
